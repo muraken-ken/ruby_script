68da601722af65102a455f0c5aa7ab70415aa313
muraken-ken <qzs04130@gmail.com>
2021-10-20 11:10:32 +0900

    Add the hit_blow.rb
    - 数当てゲーム２を追加

---
+++ lib/hit_blow.rb
@@ -0,0 +1,56 @@
+require 'readline'
+
+# 4桁の答えをセットする（先頭は0以外）
+def set_answer
+  answer = (0..9).to_a
+  while answer[0].zero? do
+    answer = answer.sample(4)
+  end
+  return answer
+end
+
+# 4桁の数字入力
+def input_number
+  input_array = []
+  stty_save = `stty -g`.chomp
+  begin
+    while input_array = Readline.readline('4桁の数字を入力してください（重複なし、先頭は0以外）> ', true).split('').map(&:to_i)
+      unless (input_array.count != 4) || input_array[0].zero? || (input_array.count - input_array.uniq.count).positive?
+        return input_array
+      end
+
+      puts '数字を入れ直してください'
+    end
+  rescue Interrupt
+    system('stty', stty_save)
+    exit
+  end
+end
+
+answer = set_answer
+hit = 0
+until hit == 4
+  input_array = input_number
+  count_answer = 0
+  count_input = 0
+  hit = 0
+  blow = 0
+  answer.each do |ans|
+    input_array.each do |input|
+      if ans == input && count_answer == count_input
+        hit += 1
+        count_input += 1
+        break
+      elsif ans == input
+        blow += 1
+        count_input += 1
+        break
+      end
+      count_input += 1
+    end
+    count_answer += 1
+    count_input = 0
+  end
+  puts "Hit:#{hit}、blow:#{blow}"
+end
+puts 'おめでとう正解です。'
